c9412004286bda2dd7978b50ecf4ef98c130f917
Craig Davis <cdavis@stoneydavis.com>
2016-11-21 09:01:56 -0600

    Enabling virtio support for kernel

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -7,6 +7,6 @@ emerge --quiet-build sys-kernel/gentoo-sources
 emerge --quiet-build sys-kernel/genkernel
 cd /usr/src/linux
 mv /tmp/kernel.config .config
-genkernel --install --symlink --no-zfs --no-btrfs --oldconfig all
+genkernel --virtio --install --symlink --no-zfs --no-btrfs --oldconfig all
 emerge --quiet-build -c sys-kernel/genkernel
 EOF
