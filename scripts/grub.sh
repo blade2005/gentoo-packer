b640c2e4e10b0d3c5dc22c67f19a73caba659806
Craig Davis <cdavis@stoneydavis.com>
2016-11-20 20:10:50 -0600

    Gentoo no longer appends for grub2 it is now just grub-*

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -4,6 +4,6 @@ chroot /mnt/gentoo /bin/bash <<'EOF'
 set -x
 emerge --quiet-build ">=sys-boot/grub-2.0"
 echo "set timeout=0" >> /etc/grub.d/40_custom
-grub2-install /dev/sda
-grub2-mkconfig -o /boot/grub/grub.cfg
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
 EOF
