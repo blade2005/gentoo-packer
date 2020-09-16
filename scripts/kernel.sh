d0d534969005a1f8d85622ea6b97b1a062258665
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 18:43:49 -0500

    Install sys-kernel/linux-firmware during compile

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -8,7 +8,7 @@ echo sys-kernel/vanilla-kernel ~amd64 >> \
 emerge app-portage/gentoolkit
 euse -p sys-kernel/vanilla-kernel -E initramfs
 cp /tmp/kernel.config /usr/src/linux/.config
-emerge sys-kernel/vanilla-kernel sys-kernel/genkernel
+emerge sys-kernel/vanilla-kernel sys-kernel/genkernel sys-kernel/linux-firmware
 cd /usr/src/linux
 #genkernel --install --symlink --no-zfs --no-btrfs --oldconfig --loglevel=1 all
 EOF
