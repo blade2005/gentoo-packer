bee3dfe7c597c41b31754a434a1e8d66cedc3237
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 15:57:15 -0500

    Fix stage3 script

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -6,5 +6,5 @@ df -h /mnt/gentoo
 sleep 10
 cd /mnt/gentoo
 wget -q -O stage3.tar.xz https://gentoo.osuosl.org/releases/amd64/autobuilds/$(curl -s https://gentoo.osuosl.org/releases/amd64/autobuilds/latest-stage3-amd64.txt | grep -v ^# |awk '{print $1}')
-tar xjpf $tarball
-rm -f $tarball
+tar xpf stage3.tar.xz
+rm -f stage3.tar.xz
