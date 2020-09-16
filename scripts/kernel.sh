a4d5daac02821db663a47c733672c956124d5d81
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 18:32:57 -0500

    Updated the kernel emerge commands to one command and copy the kernel config

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -3,10 +3,9 @@
 cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-emerge --quiet-build sys-kernel/vanilla-kernel
-emerge --quiet-build sys-kernel/genkernel
+emerge sys-kernel/vanilla-kernel sys-kernel/genkernel
 cd /usr/src/linux
-mv /tmp/kernel.config .config
+cp /tmp/kernel.config .config
 euse -p sys-kernel/vanilla-kernel -E initramfs
 echo sys-kernel/vanilla-kernel ~amd64 >> \
   /etc/portage/package.accept_keywords/kernel
