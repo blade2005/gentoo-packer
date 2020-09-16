4993809a12b440be333ec8316228fc2b686812c6
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 16:43:22 -0500

    Added default options and license accepts

--- scripts/portage.sh
+++ scripts/portage.sh
@@ -2,6 +2,9 @@
 
 cat << 'EOF' >> /mnt/gentoo/etc/portage/make.conf
 USE="$USE cron vim vim-syntax nano nano-syntax python -systemd -gtk -gnome -kde -qt4 -alsa curl lzma lz0 nmap ssl readline ncurses ruby logrotate symlink"
+EMERGE_DEFAULT_OPTS="${EMERGE_DEFAULT_OPTS} --quiet-build --quiet "
+ACCEPT_LICENSE="@FREE @BINARY-REDISTRIBUTABLE"
+
 EOF
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
