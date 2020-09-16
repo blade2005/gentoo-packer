227aaac30291f44507537be8444604a7a283a125
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 18:37:01 -0500

    changes to using the kernel build system

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -3,12 +3,11 @@
 cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-emerge sys-kernel/vanilla-kernel sys-kernel/genkernel
-cd /usr/src/linux
-cp /tmp/kernel.config .config
-euse -p sys-kernel/vanilla-kernel -E initramfs
 echo sys-kernel/vanilla-kernel ~amd64 >> \
   /etc/portage/package.accept_keywords/kernel
-genkernel --install --symlink --no-zfs --no-btrfs --oldconfig --loglevel=1 all
-emerge --quiet-build -c sys-kernel/genkernel
+euse -p sys-kernel/vanilla-kernel -E initramfs
+cp /tmp/kernel.config /usr/src/linux/.config
+emerge sys-kernel/vanilla-kernel sys-kernel/genkernel
+cd /usr/src/linux
+#genkernel --install --symlink --no-zfs --no-btrfs --oldconfig --loglevel=1 all
 EOF
