55d4c941b9961463362d2bbeba711d54e0d31914
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 16:11:52 -0500

    Added extra sync made it not quiet

--- scripts/portage.sh
+++ scripts/portage.sh
@@ -5,6 +5,7 @@ mkdir /usr/portage
 cd /etc/portage
 rm make.profile
 ln -s ../../var/db/repos/gentoo/profiles/default/linux/amd64/17.1 make.profile
-emerge-webrsync -q
+emerge-webrsync
+emerge --sync -q
 emerge  --backtrack=100 --verbose-conflicts --update --changed-use --deep --with-bdeps=y @world
 EOF
