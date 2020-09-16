9dc802cd00e574e3b09a5a39405460faa9c36e95
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 16:00:05 -0500

    Set static nameserver

--- scripts/mounts.sh
+++ scripts/mounts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /
-mount ${BLK_DEV}1 /mnt/gentoo/boot
+mount ${BLK_DEV}2 /mnt/gentoo/boot
 mount -t proc proc /mnt/gentoo/proc
 mount --rbind /dev /mnt/gentoo/dev
 mount --rbind /sys /mnt/gentoo/sys
