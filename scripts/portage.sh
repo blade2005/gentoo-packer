1baef400951f56ab55188fbbdb41d613401a3468
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 16:05:00 -0500

    fix up make profile

--- scripts/portage.sh
+++ scripts/portage.sh
@@ -2,6 +2,9 @@
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
+cd /etc/portage
+rm make.profile
+ln -s ../../var/db/repos/gentoo/profiles/default/linux/amd64/17.1 make.profile
 emerge-webrsync -q
 emerge  --backtrack=100 --verbose-conflicts --update --changed-use --deep --with-bdeps=y @world
 EOF
