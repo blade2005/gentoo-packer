a30b5bcafb1b61bb5702cab367106152c84c8711
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 18:42:47 -0500

    install gentoolkit before we use euse

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -5,6 +5,7 @@ cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
 chroot /mnt/gentoo /bin/bash <<'EOF'
 echo sys-kernel/vanilla-kernel ~amd64 >> \
   /etc/portage/package.accept_keywords/kernel
+emerge app-portage/gentoolkit
 euse -p sys-kernel/vanilla-kernel -E initramfs
 cp /tmp/kernel.config /usr/src/linux/.config
 emerge sys-kernel/vanilla-kernel sys-kernel/genkernel
