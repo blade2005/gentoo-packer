5fa7b95c295e6657c84265138c98e847e25ceddd
Craig Davis <kg5ohk@tutanota.com>
2020-09-16 21:56:57 -0500

    fix bug in fstab

--- scripts/fstab.sh
+++ scripts/fstab.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-chroot /mnt/gentoo /bin/bash <<'EOF'
-cat > /etc/fstab <<'DATA'
+cat > /mnt/gentoo/etc/fstab <<DATA
 # <fs>		<mount>	<type>	<opts>		<dump/pass>
 ${BLK_DEV}1	/boot	ext2	noauto,noatime	1 2
 ${BLK_DEV}3	/	ext4	noatime		0 1
