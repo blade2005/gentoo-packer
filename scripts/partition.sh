e39fdb1f956d8e3b4c9682e8c827e1948b20c5af
Craig Davis <kg5ohk@tutanota.com>
2020-09-17 00:53:29 -0500

    Let's not try to create a 4 partition that's not there.

--- scripts/partition.sh
+++ scripts/partition.sh
@@ -5,7 +5,7 @@ env
 sgdisk \
   -n 1:0:+128M -t 1:8300 -c 1:"linux-boot" \
   -n 2:0:+32M  -t 2:ef02 -c 2:"bios-boot"  \
-  -n 4:0:0     -t 4:8300 -c 4:"linux-root" \
+  -n 3:0:0     -t 4:8300 -c 3:"linux-root" \
   -p ${BLK_DEV}
 
 sync
