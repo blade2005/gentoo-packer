a1a7bba15a7998fc36b16d44d59c721c34c26c7c
Craig Davis <kg5ohk@tutanota.com>
2020-09-17 13:56:36 -0500

    Copy stuff to the correct location and add better things for genkernel

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
+cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/kernel.config
+cp $SCRIPTS/scripts/authorized_keys /mnt/gentoo/tmp/authorized_keys
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 echo sys-kernel/vanilla-kernel ~amd64 >> \
@@ -8,7 +9,26 @@ echo sys-kernel/vanilla-kernel ~amd64 >> \
 emerge app-portage/gentoolkit
 euse -p sys-kernel/vanilla-kernel -E initramfs
 cp /tmp/kernel.config /usr/src/linux/.config
-emerge sys-kernel/vanilla-kernel sys-kernel/genkernel sys-kernel/linux-firmware
+emerge \
+  sys-kernel/vanilla-sources \
+  sys-kernel/genkernel \
+  sys-kernel/linux-firmware
 cd /usr/src/linux
-#genkernel --install --symlink --no-zfs --no-btrfs --oldconfig --loglevel=1 all
+genkernel \
+  --install \
+  --lvm \
+  --symlink \
+  --no-zfs \
+  --no-btrfs \
+  --oldconfig \
+  --loglevel=1 \
+  --microcode=all \
+  --ssh-authorized-keys-file=/tmp/authorized_keys \
+  --ssh \
+  --kernel-config=/tmp/kernel.config \
+  --no-menuconfig \
+  --mountboot \
+  --microcode \
+  --module-rebuild \
+  all
 EOF
