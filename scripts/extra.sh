f2cb5607aba3c73d95851729a425b6407740993e
Craig Davis <kg5ohk@tutanota.com>
2020-09-17 14:40:12 -0500

    Moved commands not needing chroot out of the chroot call

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 cp $SCRIPTS/scripts/blade-recommended.set /mnt/gentoo/etc/portage/sets/blade-recommended
-chroot /mnt/gentoo /bin/bash <<'EOF'
-echo app-portage/pgo >> /etc/portage/package.accept_keywords/portage
-echo sys-kernel/vanilla-sources >> /etc/portage/package.accept_keywords/kernel
-euse -p media-libs/libglvnd -E X
-emerge --quiet-build -U --autounmask y --autounmask-write y \
-@blade-recommended
-EOF
+
+echo app-portage/pgo >> /mnt/gentoo/etc/portage/package.accept_keywords/portage
+echo sys-kernel/vanilla-sources >> /mnt/gentoo/etc/portage/package.accept_keywords/kernel
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-echo 's0:12345:respawn:/sbin/agetty -L 115200 ttyS0 vt100
-s1:12345:respawn:/sbin/agetty -L 115200 ttyS1 vt100' >> /etc/inittab
+euse -p media-libs/libglvnd -E X
+emerge \
+  -n \
+  --quiet-build \
+  -U \
+  --autounmask y \
+  --autounmask-write y \
+  @blade-recommended
 EOF
+
+echo 's0:12345:respawn:/sbin/agetty -L 115200 ttyS0 vt100
+s1:12345:respawn:/sbin/agetty -L 115200 ttyS1 vt100' >> /mnt/gentoo/etc/inittab
