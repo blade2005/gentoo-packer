189c51620afd8fc8322e1393c0093fee926d66de
Craig Davis <kg5ohk@tutanota.com>
2020-09-17 14:17:03 -0500

    remove euse and remove install of gentoolkit. don't copy kernel because

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -6,9 +6,6 @@ cp $SCRIPTS/scripts/authorized_keys /mnt/gentoo/tmp/authorized_keys
 chroot /mnt/gentoo /bin/bash <<'EOF'
 echo sys-kernel/vanilla-kernel ~amd64 >> \
   /etc/portage/package.accept_keywords/kernel
-emerge app-portage/gentoolkit
-euse -p sys-kernel/vanilla-kernel -E initramfs
-cp /tmp/kernel.config /usr/src/linux/.config
 emerge \
   sys-kernel/vanilla-sources \
   sys-kernel/genkernel \
