d8e08406534b4a8b2643cde17be79a2e131cc220
Craig Davis <kg5ohk@tutanota.com>
2020-09-17 00:10:40 -0500

    Added extra things to accept_keywords

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -2,6 +2,8 @@
 
 cp $SCRIPTS/scripts/blade-recommended.set /mnt/gentoo/etc/portage/sets/blade-recommended
 chroot /mnt/gentoo /bin/bash <<'EOF'
+echo app-portage/pgo >> /etc/portage/package.accept_keywords/portage
+echo sys-kernel/vanilla-sources >> /etc/portage/package.accept_keywords/kernel
 emerge --quiet-build -U --autounmask y --autounmask-write y \
 @blade-recommended
 EOF
