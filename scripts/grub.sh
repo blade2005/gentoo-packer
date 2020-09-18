1b9fa892dd6523d5c33ee2e52061cd2bd64fea07
Craig Davis <kg5ohk@tutanota.com>
2020-09-17 23:44:31 -0500

    Added dolvm and dosshd

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -7,6 +7,7 @@ echo "set timeout=0" >> /etc/grub.d/40_custom
 
 cat << 'EOD' >> /etc/default/grub
 GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX console=tty0 console=ttyS0,38400n8"
+GRUB_CMDLINE_LINUX_DEFAULT="dolvm dosshd"
 ## Serial console
 GRUB_TERMINAL=serial
 GRUB_SERIAL_COMMAND="serial --speed=38400 --unit=0 --word=8 --parity=no --stop=1"
