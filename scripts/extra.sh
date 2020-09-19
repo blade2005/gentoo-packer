ae2d6e841d49372a7c04508819563019d8af1860
Craig Davis <kg5ohk@tutanota.com>
2020-09-18 23:40:38 -0500

    Enable services

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -18,3 +18,10 @@ EOF
 
 echo 's0:12345:respawn:/sbin/agetty -L 115200 ttyS0 vt100
 s1:12345:respawn:/sbin/agetty -L 115200 ttyS1 vt100' >> /mnt/gentoo/etc/inittab
+
+chroot /mnt/gentoo /bin/bash << 'EOF'
+for i in acpid cronie docker sshd net.eth0 ntpd mdadm lvm libvirtd \
+libvirt-guests local sysstat virtlogd syslog-ng; do
+  rc-update add $i
+done
+EOF
