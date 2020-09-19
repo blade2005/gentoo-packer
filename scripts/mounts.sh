565c2727200eeac53d8963cd74ffcad017c12f14
Craig Davis <kg5ohk@tutanota.com>
2020-09-18 22:34:26 -0500

    Fix mounting process

--- scripts/mounts.sh
+++ scripts/mounts.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-cd /
-mount ${BLK_DEV}2 /mnt/gentoo/boot
-mount -t proc proc /mnt/gentoo/proc
-mount --rbind /dev /mnt/gentoo/dev
+mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev 
+
+chroot /mnt/gentoo /bin/bash << EOF
+mount ${BLK_DEV}2 /boot
+EOF
 
 mount
 df -h
